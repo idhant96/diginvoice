4edbb87921cc83a4a347e29848ef152674c8bd85
colaplayer <idhant123@gmail.com>
2017-12-22 17:22:13 +0530

    get_lines logic not working

--- task.py
+++ task.py
@@ -6,73 +6,129 @@ import operator
 from enchant import DictWithPWL
 from enchant.checker import SpellChecker
 import enchant
+import re
+'''
+QTY is not spell checkable
 
+'''
+# stores words with coordinates
+contents = {}
+#stores spell checked words
+check = []
+
+#  Headers Data
 data = None
 with open('data/headers.json') as fh:
     data = json.load(fh)
+
+# Init the enchant dictionaries
 dictionary = DictWithPWL("en_US")
-checker = SpellChecker(dictionary)
 a = enchant.Dict()
+
+#Adding the personal words to dict
 for element in data['table']:
     a.add(element)
 
-def spell_check(e_words):
-    if a.check(e_words) is not True:
-        if a.suggest(e_words) is not None:
-            # print("no",a.suggest(e_words)[0])
-            try:
-                return a.suggest(e_words)[0]
-            except IndexError:
-                return e_words
-        else:
-            print("here",e_words)
-            return e_words
-    return e_words
 
 
-
-
+# init the google client
 google_vision_client = vision.ImageAnnotatorClient()
 with io.open('images/first.jpg', 'rb') as image_file:
     data = image_file.read()
 vision_image = types.Image(content=data)
-p = []
-contents = {}
+
 # detect text
 response = google_vision_client.text_detection(image=vision_image)
-content = response.text_annotations
-words = content[0].description.split('\n')
-# words = words.translate(None, '!@#$",')
-checked = ' '
-with open('data/headers.json') as fh:
-    data = json.load(fh)
-for elements in words:
-    for element in elements.split(' '):
-        checked = checked + spell_check(element) + ' '
-        #     checked = spell_check(element)
-        #     #print(element)
-        #     if values == checked:
-        #         if checked not in p:
-        #             p.append(elements)
+annotations = response.text_annotations
+all_text = annotations[0].description.split('\n')
+
+def reg_cleaner(strs):
+    return re.sub(r'[(?|$|,+"”.:|!)]',r' ',strs)
+
+# Note the image needs to be processed for proper dimensions
+def get_contents():
+    for text in all_text:
+        # cleaned = reg_cleaner(text)
+        for item in text.split(' '):
+            if item and item != ' ':
+                for object in annotations:
+                    if object.description == item:
+                        vertice = object.bounding_poly.vertices
+                        contents[item] = [(vertice[0].x,vertice[0].y),
+                                          (vertice[1].x,vertice[1].y),
+                                          (vertice[2].x,vertice[2].y),
+                                          (vertice[3].x,vertice[3].y)]
 
 
-print(checked)
-for values in p:
-    for value in values.split(' '):
-        for object in content:
-            if value == object.description:
-                contents[value] = object.bounding_poly.vertices[0].x
-sorted_x = sorted(contents.items(), key=operator.itemgetter(1))
+#needs get_contents return val
+def get_lines(content_data):
+    y_min = 0
+    line = 1
+    for key in content_data.keys():
+        vertice = content_data[key]
+        if line == 1:
+            y_min = vertice[0][1]
+        current_y = vertice[0][1]
+        while True:
+            if y_min <= current_y + 5 or y_min >= current_y - 5:
+                print("line {}: ".format(line), )
+                print(key)
+                break
+            else:
+                y_min = current_y
 
-for element,_ in sorted_x:
-    for object in content:
-        if element == object.description:
-            contents[element] = object.bounding_poly.vertices
+
+
+
+
+get_contents()
+get_lines(contents)
+
+
+
+
+
+
+
+
+
+# # Checking the words to be in headers
+# def spell_check(e_words):
+#     if e_words:
+#         checker = SpellChecker(dictionary)
+#         checker.set_text(e_words)
+#         if checker.check(e_words) is False:
+#             if a.check(e_words) is False:
+#                 if a.suggest(e_words) != []:
+#                     for x in data['table']:
+#                         if x == a.suggest(e_words)[0]:
+#                             # print('corrected by a: ', e_words)
+#                             check.append(x)
+#                             break
+#                 # print('no suggestions', e_words)
+#             else:
+#                 # print('was in a', e_words)
+#                 check.append(e_words)
+#         else:
+#             for x in data['table']:
+#                 if x == e_words:
+#                     # print('corrected by a: ', e_words)
+#                     check.append(x)
+#                     break
 #
-# # print(spell_check(p))
-# print(p)
-# # print(words)
-# # print(contents)
-
-
-
+# for elements in words:
+#     for word in elements.split(' '):
+#         if word:
+#             spell_check(word)
+#
+# for values in p:
+#     for value in values.split(' '):
+#         for object in content:
+#             if value == object.description:
+#                 contents[value] = object.bounding_poly.vertices[0].x
+# sorted_x = sorted(contents.items(), key=operator.itemgetter(1))
+#
+# for element,_ in sorted_x:
+#     for object in content:
+#         if element == object.description:
+#             contents[element] = object.bounding_poly.vertices
