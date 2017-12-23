196c4a75ed20b68142f0ddef69446bcca77e2532
colaplayer <idhant123@gmail.com>
2017-12-24 01:16:09 +0530

    get_column completed

--- task.py
+++ task.py
@@ -17,11 +17,14 @@ contents = {}
 check = []
 #stores the data in form of rows
 rows = {}
+cols = {}
 
 #  Headers Data
 data = None
 with open('data/headers.json') as fh:
     data = json.load(fh)
+# with open('data/products.json') as fh:
+#     data2 = json.load(fh)
 
 # Init the enchant dictionaries
 dictionary = DictWithPWL("en_US")
@@ -72,7 +75,7 @@ def get_rows(content_data):
             y_min = (vertice[0][1] + vertice[3][1]) / 2
             rows['line{}'.format(line)] = []
         current_y = (vertice[0][1] + vertice[3][1]) / 2
-        if current_y <= (y_min + 10) and current_y >= (y_min - 10):
+        if (y_min - 10) <= current_y <= (y_min + 10):
             rows['line{}'.format(line)].append(key)
         else:
             y_min = current_y
@@ -81,27 +84,28 @@ def get_rows(content_data):
             rows['line{}'.format(line)].append(key)
 
 
-def get_cols(content_data):
-    x_min = 0
-    line = 1
-    for key in content_data.keys():
-        vertice = content_data[key]
-        if x_min == 0:
-            y_min = (vertice[0][0] + vertice[1][0]) / 2
-            rows['line{}'.format(line)] = []
-        current_y = (vertice[0][1] + vertice[3][1]) / 2
-        if current_y <= (y_min + 10) and current_y >= (y_min - 10):
-            rows['line{}'.format(line)].append(key)
-        else:
-            y_min = current_y
-            line = line + 1
-            rows['line{}'.format(line)] = []
-            rows['line{}'.format(line)].append(key)
+
+def get_column(headers_list):
+    for header in headers_list:
+        x1,x2 = contents[header][0][0],contents[header][1][0]
+        x_mid = (x1+x2) / 2
+        cols[header] = []
+        for key in contents.keys():
+            x1,x2 = contents[key][0][0],contents[key][1][0]
+            if x1 < x_mid < x2:
+                cols[header].append(key)
+
+
+
 
 
 get_contents()
 get_rows(contents)
-print(rows['line17'])
+headers = rows['line14']
+get_column(headers)
+for col in cols:
+    print('{}: '.format(col),cols[col])
+    print('')
 
 
 
@@ -110,30 +114,30 @@ print(rows['line17'])
 
 
 
-# # Checking the words to be in headers
-# def spell_check(e_words):
-#     if e_words:
-#         checker = SpellChecker(dictionary)
-#         checker.set_text(e_words)
-#         if checker.check(e_words) is False:
-#             if a.check(e_words) is False:
-#                 if a.suggest(e_words) != []:
-#                     for x in data['table']:
-#                         if x == a.suggest(e_words)[0]:
-#                             # print('corrected by a: ', e_words)
-#                             check.append(x)
-#                             break
-#                 # print('no suggestions', e_words)
-#             else:
-#                 # print('was in a', e_words)
-#                 check.append(e_words)
-#         else:
-#             for x in data['table']:
-#                 if x == e_words:
-#                     # print('corrected by a: ', e_words)
-#                     check.append(x)
-#                     break
-#
+# Checking the words to be in headers
+def spell_check(e_words):
+    if e_words:
+        checker = SpellChecker(dictionary)
+        checker.set_text(e_words)
+        if checker.check(e_words) is False:
+            if a.check(e_words) is False:
+                if a.suggest(e_words) != []:
+                    for x in data['table']:
+                        if x == a.suggest(e_words)[0]:
+                            # print('corrected by a: ', e_words)
+                            check.append(x)
+                            break
+                # print('no suggestions', e_words)
+            else:
+                # print('was in a', e_words)
+                check.append(e_words)
+        else:
+            for x in data['table']:
+                if x == e_words:
+                    # print('corrected by a: ', e_words)
+                    check.append(x)
+                    break
+
 # for elements in words:
 #     for word in elements.split(' '):
 #         if word:
