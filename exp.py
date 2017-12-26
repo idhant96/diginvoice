ee40c20159a95ce02d449fed1fe614cea55b368a
colaplayer <idhant123@gmail.com>
2017-12-26 13:48:42 +0530

    changed called functions

--- exp.py
+++ exp.py
@@ -1,27 +1,17 @@
-# import OCR file
-from working.Ocr import Ocr
-from working.Operations import Operations
-# import open cv version 3
+# import package
+from core.Ocr import Ocr
+from core.Operations import Operations
 import cv2
-
-# import json
-import json
-#
-# # get list of products
-# with open('./data/products.json') as product_file:
-#     products = json.load(product_file)['products']
-#
-#
-# # get table headers
-# with open('./data/headers.json') as product_file:
-#     common_list = json.load(open('data/headers.json'))['table']
-
 # create OCR instance
 x = Ocr()
-y = Operations()
-x.set_image('images/janata.jpg')
-blocks = x.text_detection()
-print(y.get_products(blocks))
+data1 = x.get_data('products','products')
+data2 = x.get_data('all','all')
+x.load_spellings(data2)
+x.set_image('images/first.jpg')
+annotations = x.text_detection()
+
+cropped  = x.get_roi(annotations, data2, data1)
+cv2.imwrite("{}.png".format('temp'), cropped)
 # cv2.imshow('marked',image)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
