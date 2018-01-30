de10d5405b6c98ab83ef3041c42410f60b6f60a2
idhant96 <idhant123@gmail.com>
2018-01-30 14:02:05 +0530

    added jacardian

--- dupl.py
+++ dupl.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+import distance
 
 
 class Dupl(object):
@@ -10,6 +12,9 @@ class Dupl(object):
     # print(x[x=='HEMATOLOGIST'])
     df['FIRST_NAME'] = df['FIRST_NAME'] + ' ' + df['MIDDLE_NAME'] + ' ' + df['LAST_NAME']
     score = 0
+
+    for pos, obj in df.iterrows():
+        obj.FIRST_NAME = re.sub(' +',' ',obj.FIRST_NAME)
 
     for pos, obj in df.iterrows():
         if obj.Speciality_Name not in specs_names:
@@ -26,47 +31,39 @@ class Dupl(object):
         print(spec)
         # print(names)
         input(''.format(spec))
+        print(names)
         for name1 in names:
             for name2 in names:
+                # print(name1, name2)
+
                 if names.index(name1) != names.index(name2):
                     q = name1.replace('.', ' ')
                     w = name2.replace('.', ' ')
                     words1 = q.split(' ')
                     words2 = w.split(' ')
-                    flag = 0
-                    l = 0
-                    if len(words1) > len(words2):
-                        l = len(words2)
-                        for word in words2:
-                            if word in words1:
-                                flag = flag + 1
-                    else:
-                        l = len(words1)
-                        for word in words1:
-                            if word in words2:
-                                flag = flag + 1
-                    if flag == l:
+                    jac = 1 - distance.jaccard(name1.upper(), name2.upper())
+                    if jac > 0.75:
                         # print(name1, name2)
-                        score = 28
+                        score = 60
                         pos2 = x[x == name2].index[0]
                         pos1 = x[x == name1].index[0]
                         # hs = df['HOSPITAL_ATTACHED1']
-                        role1 = df['Role'].iloc[pos1]
-                        role2 = df['Role'].iloc[pos2]
-                        if role1 == role2:
-                            score = score + 14
+                        # role1 = df['Role'].iloc[pos1]
+                        # role2 = df['Role'].iloc[pos2]
+                        # if role1 == role2:
+                        #     score = score + 15
                         pract1 = df['Practise'].iloc[pos1]
                         pract2 = df['Practise'].iloc[pos2]
                         if pract1 == pract2:
-                            score = score + 14
+                            score = score + 15
                         zone1 = df['ZONE'].iloc[pos1]
                         zone2 = df['ZONE'].iloc[pos2]
                         if zone1 == zone2:
-                            score = score + 14
+                            score = score + 15
                         ter1 = df['TERRITORY'].iloc[pos1]
                         ter2 = df['TERRITORY'].iloc[pos2]
                         if ter1 == ter2:
-                            score = score + 14
+                            score = score + 10
                         # if score > 80:
                         print(name1, name2, score, pos1, pos2)
 
