96999f17660fbfdc6c77308d1339cf901a467920
idhant96 <idhant123@gmail.com>
2018-02-01 23:43:57 +0530

    mods

--- big_dupl.py
+++ big_dupl.py
@@ -1,75 +1,89 @@
 import pandas as pd
 import re
 import distance
-
+import sys
+from fuzzywuzzy import fuzz
 
 class Big(object):
     bigdata = pd.read_csv(
-        'csvs/small_docs.csv'
+        'csvs/snall_docs.csv'
     )
+    fl = open('result.txt', 'w')
     bigdata = bigdata.fillna('')
     specs_names = {}
     for _, obj in bigdata.iterrows():
         obj.FIRST_NAME = re.sub(' +',' ',obj.DoctorName)
 
-    for pos, obj in bigdata.iterrows():
+    for _, obj in bigdata.iterrows():
         # print(obj.Specialization)
         if obj.Specialization not in specs_names:
             specs_names[obj.Specialization] = []
         else:
-            specs_names[obj.Specialization].append(pos)
-    # print(specs_names.keys())
-    specs = {}
+            specs_names[obj.Specialization].append(int(obj['Doctor Code']))
+    # print(specs_names)
+    # sys.exit())
     for spec in specs_names.keys():
-        specs[spec] = specs_names[spec]
-        if spec == 'physician1':
+        if spec == 'Paediatrician':
             break
-    # print(specs)
-    # sys.exit()
-    for spec in specs.keys():
         names = []
-        for positions in specs[spec]:
-            # print(positions)
-            names.append(bigdata.iloc[positions].DoctorName)
-        print(spec)
-        print(names)
-        # sys.exit()
-        input('first lol')
-        for name1 in names:
-            for name2 in names:
-                # print(name1, name2)
-                if names.index(name1) != names.index(name2):
-                    q = name1.replace('.', ' ')
-                    w = name2.replace('.', ' ')
-                    words1 = q.split(' ')
-                    words2 = w.split(' ')
-                    print(name1, name2)
+        # input('first lol')
+        for val1 in specs_names[spec]:
+            for val2 in specs_names[spec]:
+                if val1 != val2:
+                    name1 = bigdata.loc[bigdata['Doctor Code'] == val1, 'DoctorName'].iloc[0]
+                    name2 = bigdata.loc[bigdata['Doctor Code'] == val2, 'DoctorName'].iloc[0]
+                    n1 = name1.split(' ')
+                    n2 = name2.split(' ')
+                    jac_sum = 0
+                    # if len(n1) <= len(n2):
+                    #     for words1 in n1:
+                    #         for words2 in n2:
+                    #             d = 1 - distance.jaccard(words1.upper(), words2.upper())
+                    #             jac_sum = jac_sum + d
+                    # elif len(n1) > len(n2):
+                    #     for words1 in n2:
+                    #         for words2 in n1:
+                    #             d = 1 - distance.jaccard(words1.upper(), words2.upper())
+                    #             jac_sum = jac_sum + d
+                    # lent = len(n1)*len(n2)
+                    # print(name1, name2, lent, (jac_sum/lent)*100)
+                    # if (jac_sum/lent)*100 > 36:
+                    #     input('check')
+                    # continue
+                    words1 = name1.replace('.', ' ').split(' ')
+                    words2 = name2.replace('.', ' ').split(' ')
                     jac = 1 - distance.jaccard(name1.upper(), name2.upper())
-                    if jac > 0.75:
-                        input('entered')
+                    f = fuzz.partial_ratio(name1.upper(), name2.upper())
+                    pos1 = bigdata.loc[bigdata['Doctor Code'] == val1].index[0]
+                    pos2 = bigdata.loc[bigdata['Doctor Code'] == val2].index[0]
+                    age1 = bigdata['Age'].iloc[pos1]
+                    age2 = bigdata['Age'].iloc[pos2]
+                    print(name1, name2, age1, age2)
+                    if age1 == age2 and f > 70:
+                        # input('entered')
                         # print(name1, name2)
-                        score = 55
-                        pos1 = bigdata['DoctorName'][bigdata['DoctorName'] == name1].index[0]
-                        pos2 = bigdata['DoctorName'][bigdata['DoctorName'] == name2].index[0]
-                        print(name1, pos1, name2, pos2)
+                        score = 70
+                        # print(name1, pos1, name2, pos2, jac)
                         # sys.exit()
                         gender1 = bigdata['Sex'].iloc[pos1]
                         gender2 = bigdata['Sex'].iloc[pos2]
                         print(gender1, gender2)
                         if gender1 == gender2:
                             score = score + 15
-                        age1 = bigdata['Age'].iloc[pos1]
-                        age2 = bigdata['Age'].iloc[pos2]
+
                         print(age1, age2)
-                        if age1 == age2:
-                            score = score + 15
+
                         zone1 = bigdata['CityName'].iloc[pos1]
                         zone2 = bigdata['CityName'].iloc[pos2]
                         print(zone1, zone2)
                         if zone1 == zone2:
                             score = score + 15
-                        print(score)
-                        input('scheck score')
-                        if score > 80:
-                            print(name1, name2, score, pos1, pos2)
-                            input('lol')
+                        # print(score)
+                        # input('scheck score')
+                        if score > 85:
+                            # print(name1, name2, score, pos1, pos2)
+                            s = name1 + ' ' + name2 + ' ' + str(f) + '\n'
+                            print(s)
+                            fl.write(s)
+                            # input('lol')
+    fl.close()
